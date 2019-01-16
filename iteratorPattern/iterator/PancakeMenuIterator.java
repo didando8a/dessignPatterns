0212a4783e38028bad6751b1ea17bbf9e5998880
Diego Daniel Dorado Ochoa <didando8a@gmail.com>
2019-01-16 01:15:18 +0000

    Add Iterators

---
+++ iteratorPattern/iterator/PancakeMenuIterator.java
@@ -0,0 +1,22 @@
+package iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class PancakeMenuIterator implements Iterator {
+    ArrayList<MenuItem> items;
+    Iterator<MenuItem> iterator;
+
+    public PancakeMenuIterator(ArrayList<MenuItem> items) {
+        this.items = items;
+        this.iterator = this.items.iterator();
+    }
+
+    public boolean hasNext() {
+        return iterator.hasNext();
+    }
+
+    public Object next() {
+        return iterator.next();
+    }
+}
